b5c93a7c3d04d73704cca44bdfe5f099e7c4dff8
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-09-14 18:50:38 -0300

    atualizado discount para 30 na tabela order detail o items com unique_price menor que 100000 conforme especificado no desafio 23

---
+++ desafio23.sql
@@ -0,0 +1,1 @@
+UPDATE northwind.order_details SET discount = 30 WHERE unit_price < 10.0000;
--- tests/tableManipulationChallenges.spec.js
+++ tests/tableManipulationChallenges.spec.js
@@ -100,7 +100,7 @@ describe('Desafios de manipulação de tabelas', () => {
       });
     });
 
-    describe("23 - Atualize os dados da coluna 'discount' da tabela 'order_details' para 30, onde o valor na coluna 'unit_price' seja menor que 10.0000", () => {
+    describe.only("23 - Atualize os dados da coluna 'discount' da tabela 'order_details' para 30, onde o valor na coluna 'unit_price' seja menor que 10.0000", () => {
       it('Verifica o desafio23', async () => {
         const challengeQuery = readFileSync('desafio23.sql', 'utf8').trim();
 
