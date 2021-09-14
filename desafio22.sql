3412be6a76cbca9527de9ada74651fd12bd2798a
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-09-14 18:36:41 -0300

    atualizado discount para 15 na tabela order detail conforme especificado no desafio 22

---
+++ desafio22.sql
@@ -0,0 +1,1 @@
+UPDATE northwind.order_details SET discount = 15;
--- tests/tableManipulationChallenges.spec.js
+++ tests/tableManipulationChallenges.spec.js
@@ -86,7 +86,7 @@ describe('Desafios de manipulação de tabelas', () => {
     const countOrderDetailsByDiscountQuery = (discount) =>
       `SELECT COUNT(*) AS details_count FROM order_details WHERE discount = ${discount};`;
 
-    describe("22 - Atualize os dados na coluna 'discount' da tabela 'order_details' para 15", () => {
+    describe.only("22 - Atualize os dados na coluna 'discount' da tabela 'order_details' para 15", () => {
       it('Verifica o desafio22', async () => {
         const challengeQuery = readFileSync('desafio22.sql', 'utf8').trim();
 
